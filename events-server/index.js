7233d329b296b5162eca59abc079cf3456dd1caa
derek williams <derek61@gmail.com>
2022-10-28 12:37:19 -0600

    copied over from handbook examples

---
+++ events-server/index.js
@@ -0,0 +1,38 @@
+const pg = require('pg')
+const express = require('express')
+const { postgraphile } = require('postgraphile')
+const ConnectionFilterPlugin = require("postgraphile-plugin-connection-filter");
+
+require('dotenv').config()
+const app = express()
+const cors = require('cors');
+
+const pgPool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL
+})
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+
+app.use(
+  postgraphile(
+    pgPool,
+    process.env.SCHEMA_NAMES ? process.env.SCHEMA_NAMES.split(',') : ['public'],
+    {
+      appendPlugins: [ConnectionFilterPlugin],
+      watchPg: true,
+      graphiql: true,
+      enhanceGraphiql: true,
+      dynamicJson: true,
+      enableCors: true,
+      allowExplain(req) {
+        return true
+      },
+    }
+  )
+)
+const port = process.env.PORT || 4002
+app.listen(port)
+console.log(`🚀 Server ready at http://[host]:${port}/graphql`)
+console.log(`🚀 Graphiql UI ready at http://[host]:${port}/graphiql`)

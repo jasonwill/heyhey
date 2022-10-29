01b15af20826009ee10d638e54176575148041c8
derek williams <derek61@gmail.com>
2022-10-29 16:48:27 -0600

    playing with stitching

--- stitching/index.js
+++ stitching/index.js
@@ -7,31 +7,72 @@ const { stitchSchemas } = require('@graphql-tools/stitch');
 const makeRemoteExecutor = require('./lib/make_remote_executor');
 
 async function makeGatewaySchema() {
-  const usersExec = makeRemoteExecutor('http://localhost:4001/graphql');
-  const eventsExec = makeRemoteExecutor('http://localhost:4002/graphql');
-  const announcementsExec = makeRemoteExecutor('http://localhost:5002/graphql');
 
-  
+  const usersRemoteQueryExec = makeRemoteExecutor('http://localhost:4001/graphql');
+  const announcementsRemoteQueryExec = makeRemoteExecutor('http://localhost:5002/graphql');
+  const eventsRemoteQueryExec = makeRemoteExecutor('http://localhost:4002/graphql');
+
+  const usersSchema = await introspectSchema(usersRemoteQueryExec);
+  const announcementsSchema = await introspectSchema(announcementsRemoteQueryExec);
+  const eventsSchema = await introspectSchema(eventsRemoteQueryExec);
+
   return stitchSchemas({
     subschemas: [
       {
-        schema: await introspectSchema(usersExec),
-        executor: usersExec,
+        schema: announcementsSchema,
+        executor: announcementsRemoteQueryExec,
+        merge: {
+          User: {
+            //Announcement.userByAuthorId
+            fieldName: 'userByAuthorId',
+            selectionSet: '{ id }',
+            args: originalObject => ({ id: originalObject.id }),
+          },
+          Event: {
+            fieldName: 'eventByEventId',
+            selectionSet: '{ id }',
+            key: ({ id }) => id,
+            args: originalObject => ({ id: originalObject.id }),
+          },
+          Announcement: {
+            //Announcement.eventByEventId
+            fieldName: 'announcementsByEventId',
+            selectionSet: '{ id }',
+            key: ({ id }) => id,
+            args: originalObject => ({ id: originalObject.id }),
+          }
+        }
       },
       {
-        schema: await introspectSchema(eventsExec),
-        executor: eventsExec,
+        schema: eventsSchema,
+        executor: eventsRemoteQueryExec,
+        merge: {
+          Event: {
+            fieldName: 'eventById',
+            selectionSet: '{ id }',
+            args: originalObject => ({ id: originalObject.id }),
+          },
+        }
       },
       {
-        schema: await introspectSchema(announcementsExec),
-        executor: announcementsExec,
+        schema: usersSchema,
+        executor: usersRemoteQueryExec,
+        merge: {
+          User: {
+            fieldName: 'userById',
+            selectionSet: '{ id }',
+            args: originalObject => ({ id: originalObject.id }),
+          }
+        }
       }
     ],
+    mergeTypes: true // << default in v7
   });
 }
 
-waitOn({ resources: ['tcp:4001', 'tcp:4002'] }, async () => {
+waitOn({ resources: ['tcp:4001', 'tcp:4002', 'tcp:5002'] }, async () => {
   const schema = await makeGatewaySchema();
+  // debugger;
   const app = express();
   app.use('/graphql', graphqlHTTP((req) => ({
     schema,

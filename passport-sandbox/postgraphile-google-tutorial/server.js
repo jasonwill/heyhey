72209eabd8b08a2a72f020376b0e2fe53d773e06
derek williams <derek61@gmail.com>
2023-01-06 11:09:46 -0700

    added session support

--- passport-sandbox/postgraphile-google-tutorial/server.js
+++ passport-sandbox/postgraphile-google-tutorial/server.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 
 const createError = require('http-errors');
+const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors");
-var path = require('path');
+const path = require('path');
 
 const pg = require('pg')
-const express = require('express')
 const session = require('express-session');
 const passport = require('passport');
 const indexRouter = require('./routes/index');
@@ -46,11 +46,26 @@ app.use(
 );
 
 // see https://www.passportjs.org/tutorials/google/session
-app.use(passport.authenticate('session')); // TODO use this
+app.use(passport.authenticate('session')); // TODO use this, but right now we get an error when trying to hit the graphql server with this line
 // end see https://www.passportjs.org/tutorials/google/session
 
 app.use('/', indexRouter);
 app.use('/', authRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 const pgPool = new pg.Pool({
   connectionString: process.env.DATABASE_URL

2fd87b103ddda0f6d69bf93bf87c305f0e072fdf
derek williams <derek61@gmail.com>
2023-02-10 08:46:43 -0700

    remove old tests

--- heyhey-client-open-props/src/__tests__/components/MainNavigation.spec.js
+++ heyhey-client-open-props/src/__tests__/components/MainNavigation.spec.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { MainNavigation } from '../../components/MainNavigation';
 
 test('renders learn react link', () => {
-  const { container } = render(<MainNavigation />);
+  render(<MainNavigation />);
   const linkElement = screen.getByRole('main-navigation');
   expect(linkElement).toBeInTheDocument();
 });

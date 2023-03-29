02ddb8fd2ec9473bf89f930e1ec9aaab43c72e17
Derek <derek61@gmail.com>
2023-03-29 11:52:33 -0600

    Added comments table

--- heyhey-server/db/create.sql
+++ heyhey-server/db/create.sql
@@ -8,6 +8,7 @@ CREATE TABLE announcements.announcers (
 );
 
 
+
 CREATE TABLE announcements.announcements (
 	id bigserial NOT NULL,
 	author_id int8 NULL,
@@ -17,9 +18,25 @@ CREATE TABLE announcements.announcements (
 	updated_at timestamp(6) NOT NULL,
 	CONSTRAINT announcements_pkey PRIMARY KEY (id)
 );
-CREATE INDEX index_announcements_on_author_id ON announcements.announcements USING btree (author_id);
 
+CREATE INDEX index_announcements_on_author_id ON announcements.announcements USING btree (author_id);
 -- announcements.announcements foreign keys
 
 ALTER TABLE announcements.announcements ADD CONSTRAINT announcements_fk FOREIGN KEY (author_id) REFERENCES announcements.announcers(id);
+
+CREATE TABLE announcements.comments (
+	id bigserial NOT NULL,
+	announcement_id int8 NULL,
+	"content" text NULL,
+	created_at timestamp(6) NOT NULL,
+	updated_at timestamp(6) NOT NULL,
+	CONSTRAINT comments_pkey PRIMARY KEY (id)
+);
+
+
+CREATE INDEX index_comments_on_announcement_id ON announcements.comments USING btree (announcement_id);
+
+-- announcements.announcements foreign keys
+
+ALTER TABLE announcements.announcements ADD CONSTRAINT comments_fk FOREIGN KEY (announcement_id) REFERENCES announcements.announcement(id);
 
